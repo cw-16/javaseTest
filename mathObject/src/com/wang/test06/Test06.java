8d20597803a18ec5e2f41d4ea6392f5794936100
Chen Wang <193133684@qq.com>
2020-11-26 15:42:16 +0800

    筛选字符串

---
+++ mathObject/src/com/wang/test06/Test06.java
@@ -0,0 +1,47 @@
+package com.wang.test06;
+
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+public class Test06 {
+    public static void main(String[] args) {
+//      筛选字符串。
+//
+//* 定义ArrayList集合，存入多个字符串。
+//* 长度大于5的字符串，打印删除后的集合。
+
+        ArrayList<String> list=new ArrayList<>();
+        list.add("who");
+        list.add("1234");
+        list.add("123");
+        list.add("123456");
+        list.add("abcdef");
+        list.add("love");
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if(s.length()>5){
+                list.remove(i);
+                i--;
+            }
+        }
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println(list.size());
+
+
+
+
+
+    }
+
+}
